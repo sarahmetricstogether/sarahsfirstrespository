090482225bc9d4b6d92161f7bb048dd6f91277ef
sarahmetricstogether <sarah@metricstogether.com>
2022-04-28 16:27:06 -0400

    Updated

--- A_Code_Script.R
+++ A_Code_Script.R
@@ -1,16 +1,17 @@
-#Welcome to R Sarah Barnett, you beautiful beast
+# Welcome to R Sarah Barnett, you beautiful beast
 # here's a little demo on tribal plots!
 library(tigris)
+tribalarea <- tigris::native_areas()
 ourtribes <- c("Pojoaque","Ohkay Owingeh","Taos","Picuris","Nambe","Jicarilla Apache Nation","Santa Clara","San Idelfonso","Tesuque")
 ourareas <- tribalarea[tribalarea$NAME %in% ourtribes,]
-plot(ourareas[3,16],col="lightblue")
+plot(tribalarea[,16],col="lightblue")
 
-for (i in 1:6) {
-  png(file= paste0(ourtribes[i],".png"),
-      width=600, height=350)
-  plot(ourareas[i,16],col="lightblue")
-  dev.off()
-}
+# for (i in 1:6) {
+#   png(file= paste0(ourtribes[i],".png"),
+#       width=600, height=350)
+#   plot(ourareas[i,16],col="lightblue")
+#   dev.off()
+# }
 
 plot(tribalarea[tribalarea$NAME=="San Ildefonso",16],col="lightblue")
 
